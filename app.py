7299895c457e134058b87ded3ec14a3658ddf62d
andrekmeyer <andre@nsasia.com>
2025-10-22 20:21:17 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Whisper app placeholder - upgrading instance."
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets this env var
+    app.run(host="0.0.0.0", port=port)
